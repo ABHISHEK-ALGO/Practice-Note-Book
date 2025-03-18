2e4d73f16af9da45c09c047cb4c4d1f4176f2f28
Abhishek Malviya <abhishekh.813@gmail.com>
2025-03-18 23:56:04 +0530

    new files sql

--- SALES/Basic- medium calculations.sql
+++ SALES/Basic- medium calculations.sql
@@ -1,75 +1,4 @@
-
-
-use geeks;
-show tables;
-SELECT * FROM SAMPLESTORE;
-
-/** WILDCARDS **/
-SELECT * FROM SAMPLESTORE WHERE `SHIP DATE`  LIKE "%-11-%" ; /**ESCAPE TO TREAT WILDCARDS AS LITERALS**/
-
-/**  calculate the total transaction amount for each customer for
- the current year. The output should contain Customer_Name and the total amount.**/
- SELECT `Customer Name`, SUM(`Sales`) as TOTAL_SALES
- FROM SAMPLESTORE 
- WHERE YEAR(`Order Date`)=YEAR(CURDATE())-8
- GROUP BY `Customer Name`;
- 
- use geeks;
- SELECT * FROM SAMPLESTORE;
- SELECT MAX(PROFIT) AS MAXIMUM_PRO_FURNI From  SAMPLESTORE
- WHERE CATEGORY = 'Furniture';
- 
- SELECT format(CATEGORY,4) FROM SAMPLESTORE;
- 
- SELECT `ORDER ID`,MID(`ORDER ID`,9,14) AS ORDER_NUM , `Order Date`, NOW() AS PRESENT_TIME
- FROM SAMPLESTORE;
- 
- /** count of distinct UNOQUE records **/
- select COUNT(`Order ID`) AS ORIGNAL ,COUNT(distinct(`Order ID`)) AS CNT_UNIQUE FROM SAMPLESTORE;
- 
- /**DUPLICATE RECORDDS **/
- SELECT * FROM SAMPLESTORE;
- SELECT `City` FROM SAMPLESTORE
- GROUP BY `City` having count(*)=1;
- 
- SELECT column1, column2, COUNT(*)
-FROM table_name
-GROUP BY column1, column2
-HAVING COUNT(*) > 1;
-
- --  identify duplicate records in a table based on specific columns where both cust and order id same--
-SELECT `Customer ID`, `Order ID`, COUNT(*) AS Duplicate_Count
-FROM SAMPLESTORE
-GROUP BY `Customer ID`,`Order ID`
-HAVING COUNT(*) > 1;
-
--- return only one record per duplicate group, --
-DELETE FROM SAMPLESTORE WHERE (`Customer ID` ,`Order ID`) IN (
-SELECT `Customer ID`, `Order ID`, COUNT(*) AS Duplicate_Count
-FROM SAMPLESTORE
-GROUP BY `Customer ID`,`Order ID`
-HAVING COUNT(*) > 1);
-SELECT `Customer ID`, `Order ID`, MAX(`Row ID`) AS Latest_Order
-FROM SAMPLESTORE
-GROUP BY `Customer ID`, `Order ID`;
-
--- ELIMINATE DUPLICATE--
-DELETE FROM samplestore WHERE`Customer ID` NOT IN 
-(
-SELECT `Customer ID` FROM 
-(
-SELECT MIN(`Customer ID`) as Uniq FROM samplestore GROUP BY `Customer Name`) as dist
-);
- 
- 
- 
- SELECT `CATEGORY` FROM SAMPLESTORE
- GROUP BY `CATEGORY` having sum(Quantity) > 2;
- select * from samplestore;
- SELECT  `CUSTOMER ID`,`CUSTOMER NAME`,COUNT(*) AS CNT FROM SAMPLESTORE GROUP BY `CUSTOMER ID`,`CUSTOMER NAME`;
- 
-
- /** MOM sales **/
+/** MOM sales **/
 WITH MonthlySales AS (SELECT  date_format(`Order Date`,'%Y-%m') as MONTH, SUM(Sales) AS TOTAL_SALES
 FROM SAMPLESTORE GROUP BY DATE_FORMAT(`Order Date`,'%Y-%m')
 ),
@@ -78,8 +7,7 @@ LAG(Total_Sales) OVER (ORDER BY Month) AS Previous_Sales
 FROM MonthlySales)
 SELECT Month, Total_Sales,
 (CASE
-
-	WHEN Previous_Sales IS NOT NULL 
+    WHEN Previous_Sales IS NOT NULL 
     THEN 
 		((Total_Sales - Previous_Sales) / Previous_Sales) * 100
 ELSE NULL
@@ -92,8 +20,6 @@ FROM SAMPLESTORE ;
 
 SELECT `PRODUCT NAME` , SUM(SALES) AS TOTAL_SALES FROM SAMPLESTORE 
 group by `PRODUCT NAME` ORDER BY TOTAL_SALES;
-
-
 
 /** calculate yearly growth in sales **/
 WITH YEARLY_SALES AS (SELECT EXTRACT(YEAR FROM `ORDER DATE`) AS YEAR ,
@@ -147,5 +73,13 @@ ORDER BY
 /** CTE **/
 use geeks;
 select * from samplestore;
+-- FIND GREATER THAN 200 SALES FROM CORPORATE ONLY 
 WITH CUST_COR AS (SELECT * FROM SAMPLESTORE WHERE SEGMENT = 'Corporate')
 SELECT `Order ID`,`City` from CUST_COR WHERE SALES > '200' ORDER  BY SALES;
+
+SELECT `Order ID`,`City` FROM SAMPLESTORE WHERE SALES > '200' AND SEGMENT = 'Corporate';
+
+-- Find the third highest sale from the samplestore table using DENSE_RANK--
+WITH SALERANK AS (SELECT SALES ,DENSE_RANK() OVER (ORDER BY SALES DESC) AS RANK_SALE
+FROM SAMPLESTORE)
+SELECT SALES FROM SALERANK WHERE RANK_SALE = 3;
--- SALES/JOINS.sql
+++ SALES/JOINS.sql
@@ -3,13 +3,26 @@ USE CAMPUSX;
 SHOW TABLES;
 SELECT*FROM HR_EMP_TABLE;
 SELECT*FROM PROJ_ASSIGN;
+-- MANAGERS HAVING MORE THAN 2 REPORTEE-- 
+SELECT M.FIRST_NAME
+FROM HR_EMP_TABLE M 
+INNER JOIN HR_EMP_TABLE E
+ON E.`MANAGER ID` = M.EMP_ID
+GROUP BY M.`MANAGER ID`, M.FIRST_NAME
+HAVING COUNT(E.EMP_ID) >=2;
 
 /*EMP ASSIGNED TO PROJECT FROM HEAELTHCARE DEPARTMENT*/
 SELECT E.EMP_ID,P.PROJ_ID,E.FIRST_NAME,E.DEPT FROM HR_EMP_TABLE E
 INNER JOIN PROJ_ASSIGN P ON E.EMP_ID = P.EMP_ID
 WHERE DEPT IN ('HEALTHCARE');
 
-/* Your manager wants the details of the ongoing projects along with the  number of employees working on them.*/
+/* Your manager wants the details of the ongoing projects along with the  number of employees working on them.
+A LEFT JOIN returns all records from the left table, and matching records from the right table. If there is no match, NULLs are returned.
+
+Use Cases of LEFT JOIN
+✅ Finding unmatched data (e.g., users who haven’t registered)
+✅ Retrieving all data from one table, even if there's no match in the second
+✅ Handling optional relationships (e.g., customers who haven’t made a purchase)*/
 SELECT * FROM PROJ_RECORDS;
 SELECT P.PROJ_ID,R.PROJ_NAME, R.STATUS , R.DOMAIN FROM PROJ_RECORDS R
 LEFT JOIN PROJ_ASSIGN P USING (PROJ_ID);
@@ -24,7 +37,15 @@ GROUP BY E.EMP_ID,E.FIRST_NAME, E.DEPT,E.ROLE
 ORDER BY PROJ_COUNT;
 
 /* Your manager wants you to identify the number of employees 
-reporting to each manager including the President and the CEO.*/
+reporting to each manager including the President and the CEO.
+An INNER JOIN returns only the matching rows between two tables based on the given condition.
+
+Use Cases of INNER JOIN
+✅ Retrieving only matching records
+✅ Ensuring data integrity by excluding unmatched records
+✅ When you only care about linked data and not missing values
+
+*/
 SELECT E.FIRST_NAME,COUNT(E.EMP_ID) AS EMP_COUNT , R.ROLE 
 FROM HR_EMP_TABLE E
 INNER JOIN HR_EMP_TABLE R
@@ -34,4 +55,38 @@ WHERE E.ROLE IN ("MANAGER","PRESIDENT","CEO")
 GROUP BY E.EMP_ID, E.FIRST_NAME,R.ROLE
 ORDER BY E.EMP_ID;
 
- 
+/**SELF JOIN TO COMPARE WITHIN DIFFERENT ROWS OF SAME TABLE FIND CUST ID WHERE ORDER VALUE GREATER THAN PREVIOUSA 
+SELF JOIN is a join where a table is joined with itself. It is used when we need to compare rows within the same table.
+
+Use Cases of SELF JOIN
+✅ Finding hierarchical relationships (e.g., employees and managers in the same table)
+✅ Finding duplicate or related records
+✅ Comparing values within the same table**/
+SELECT * FROM SWIGGY_SALES;
+SELECT S1.CUSTOMER_ID,S1.ORDER_DATE,S1.ORDER_VALUE
+FROM SWIGGY_SALES S1
+LEFT JOIN SWIGGY_SALES S2
+ON S2.ORDER_DATE = DATE_SUB(S1.ORDER_DATE ,INTERVAL 1 DAY)
+WHERE S1.ORDER_VALUE > S2.ORDER_VALUE
+ORDER BY ORDER_DATE; 
+
+-- PERCENTAGE OF ORDERS BY CUSTOMERS-- 
+WITH TOTALORDERS AS (SELECT COUNT(`ORDER ID`) AS TOTAL_ORDERS FROM SAMPLESTORE)
+SELECT S.`CUSTOMER ID`, ROUND((COUNT(`ORDER ID`)/(T.TOTAL_ORDERS)*100),2) AS percentage
+FROM SAMPLESTORE S 
+CROSS JOIN TOTALORDERS T
+GROUP BY S.`CUSTOMER ID`,T.TOTAL_ORDERS;
+ /**A CROSS JOIN produces the Cartesian product of two tables, meaning every row from the first table is paired with every row from the second table.
+ To Generate All Possible Combinations:
+Used when you need every row from one table paired with every row from another.
+To Combine with Aggregation:
+As in your problem, CROSS JOIN helps us get the total number of users without needing a JOIN condition.*/
+
+
+
+
+
+
+
+
+
